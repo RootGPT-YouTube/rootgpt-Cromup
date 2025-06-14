f844831ef67b4c31d151d79c4ae2ee9f097f2205
RootGPT <166707820+RootGPT-YouTube@users.noreply.github.com>
2025-06-14 11:21:39 +0200

    Modifiche link e cromup
    Ancora modifiche a link.menu.sh e a cromup

--- link_menu.sh
+++ link_menu.sh
@@ -6,6 +6,7 @@ EXEC_PATH="$HOME/Cromite/chrome"
 ICON_URL="https://camo.githubusercontent.com/6b4ee03be91712db2d81b603a1bb83553e97b66fa49443bf27b641089ea51696/68747470733a2f2f7777772e63726f6d6974652e6f72672f6170705f69636f6e2e706e67"
 ICON_PATH="$HOME/Cromite/cromite_icon.png"
 DESKTOP_FILE="$HOME/.local/share/applications/${APP_NAME}.desktop"
+DESKTOP_FILE2="$HOME/.local/share/applications/${APP_NAME}.desktop"
 
 # Scaricare l'icona (senza creare la cartella)
 curl -o "$ICON_PATH" "$ICON_URL"
@@ -27,6 +28,23 @@ X-KDE-SubstituteUID=false
 X-KDE-Username=
 Categories=Network;WebBrowser;
 " | sudo tee "$DESKTOP_FILE"
+
+echo "[Desktop Entry]
+Comment=Take back your browser
+Exec=$HOME/Cromite/cromite
+GenericName=Browser
+Icon=$HOME/Cromite/cromite_icon.png
+Name=Cromite
+NoDisplay=false
+Path=
+StartupNotify=true
+Terminal=false
+TerminalOptions=
+Type=Application
+X-KDE-SubstituteUID=false
+X-KDE-Username=
+Categories=Network;WebBrowser;
+" | sudo tee "$DESKTOP_FILE2"
 
 # Rendere il file eseguibile
 # sudo chmod a+x "$DESKTOP_FILE"
