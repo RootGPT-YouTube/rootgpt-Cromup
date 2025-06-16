fce226a11e2e3a65c2623d8934644e9bbbedb012
RootGPT <166707820+RootGPT-YouTube@users.noreply.github.com>
2025-06-16 16:16:19 +0200

    Update link_menu.sh

--- link_menu.sh
+++ link_menu.sh
@@ -55,5 +55,7 @@ sudo mv ~/Cromite/chrome ~/Cromite/cromite
 echo -e "\e[1;32mCollegamento creato con successo in $DESKTOP_FILE\e[0m"
 echo -e "\e[1;32mIcona scaricata e salvata in $ICON_PATH\e[0m"
 echo -e "\e[1;32mPremere un tasto per terminare lo script e iniziare a usare Cromite Browser!\e[0m"
-echo -e "\e[1;32mPS: bug conosciuti: in KDE l'icona nel menù va creata con l'Editor del menù (kmenuedit).\e[0m"
+echo -e "\e[1;32mPS: bug conosciuti:\e[0m"
+echo -e "\e[1;32m1. Sometimes, the icon in the menu must be created manually, I don't know why... However, the executable are in ~/Cromite/cromite;\e[0m"
+echo -e "\e[1;32m2. In some DEs, the Chromium icon will appear instead of the Cromite one.\e[0m"
 read
